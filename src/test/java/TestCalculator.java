1e79302829a01540df34903cefdee471453974ae
kbprasad1978 <kbprasad1978@gmail.com>
2025-04-04 14:26:44 +0100

    This is my first commit for BDD framework

---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,12 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+public class TestCalculator {
+
+    @Test
+    public void testAdd(){
+        Calculator c = new Calculator();
+        Assert.assertEquals(7,c.add(2,5));
+    }
+
+}
